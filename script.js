4083412cddb21be8cfe7556eb14abbd2c30c626a
02Patrick02 <54435353+02Patrick02@users.noreply.github.com>
2022-03-24 23:40:35 +0100

    Update script.js
    Added small comments and some indicator if it is ok to place ship, clearing localstorage on reload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.body.onload = init;    
+document.body.onload = init;  
 
 let hitCounter = 0;    
 let hitCounterElem = document.getElementById("hitCounter");
@@ -6,43 +6,58 @@ let hitCounterElem = document.getElementById("hitCounter");
 function init(){
     guessingBoard();
     placingBoard();
+
+    window.localStorage.clear();
 }
 
 
 function guessingBoard(){
-    let battleship = document.getElementById("battleship");
+    let guess = document.getElementById("guess");
     let counter = 0;
     
-    for(let i = 0;i <= 9;i++){
-        for(let j = 0;j <= 9;j++){
+    for(let i = 0;i <= 9;i++){ //Creating x blocks for gameboard
+        for(let j = 0;j <= 9;j++){ //Creating y blocks for gameboard
             let block = document.createElement("div");
-            battleship.appendChild(block);
-            
+            guess.appendChild(block);
+
             block.id = counter++;
             
-            let xBlock = i * 40;
+            let xBlock = i * 40; //size for the blocks
             let yBlock = j * 40; 
             
             block.style.top = xBlock + "px";
             block.style.left = yBlock + "px";
             block.style.backgroundColor = "aqua";
-
+            
             block.addEventListener("click", () => {
                 block.style.backgroundColor = "red";
-                console.log(block.id);    
-                
-                if(block.id == localStorage.getItem("id", block.id) || block.id == localStorage.getItem("nextId", block.id) ||  block.id == localStorage.getItem("prevId", block.id) ){
-                    block.style.backgroundColor = "green";
-                    hitCounterElem.innerHTML = "Score: " + hitCounter;
+
+                if(block.style.backgroundColor == "green"){ //return if already clicked
+                    return;
                 }
-                if(block.id == localStorage.getItem("ship2Id1", block.id) || block.id == localStorage.getItem("ship2Id2", block.id) ||  block.id == localStorage.getItem("ship2Id3", block.id) ){
+
+                //getting the saved value from localstorage
+                if(block.id == localStorage.getItem("id", block.id) ||  
+                block.id == localStorage.getItem("clickId", block.id) || 
+                block.id == localStorage.getItem("nextId", block.id) ||  
+                block.id == localStorage.getItem("prevId", block.id) ){
                     block.style.backgroundColor = "green";
-                    hitCounterElem.innerHTML = "Score: " + hitCounter;
                 }
-                if(block.id == localStorage.getItem("ship3Id1", block.id) || block.id == localStorage.getItem("ship3Id2", block.id) ||  block.id == localStorage.getItem("ship3Id3", block.id) ){
+
+                //getting value for button 2 from localstorage
+                if(block.id == localStorage.getItem("ship2Id1", block.id) ||
+                   block.id == localStorage.getItem("ship2Id2", block.id) ){
                     block.style.backgroundColor = "green";
-                    hitCounterElem.innerHTML = "Score: " + hitCounter;
                 }
+
+                //button 3
+                if(block.id == localStorage.getItem("ship3Id1", block.id) || 
+                block.id == localStorage.getItem("ship3Id2", block.id) ||  
+                block.id == localStorage.getItem("ship3Id3", block.id) ){
+                    block.style.backgroundColor = "green";
+                }
+
+                //adding score
                 if(block.style.backgroundColor == "green"){
                     hitCounter++;     
                     hitCounterElem.innerHTML = "Score: " + hitCounter;
@@ -53,16 +68,17 @@ function guessingBoard(){
 }
 
 function placingBoard(){
-    let battleship = document.getElementById("placingships");
+    let place = document.getElementById("placingships");
     let ship1 = document.getElementById("shipsBtn1");
     let ship2 = document.getElementById("shipsBtn2");
     let ship3 = document.getElementById("shipsBtn3");
+    
     let counter = 0;
-
+    
     for(let i = 0;i <= 9;i++){
         for(let j = 0;j <= 9;j++){
             let block = document.createElement("div");
-            battleship.appendChild(block);
+            place.appendChild(block);
             
             block.id = counter++;
             
@@ -74,47 +90,84 @@ function placingBoard(){
             block.style.backgroundColor = "aqua";
             
             ship1.addEventListener("click", () => {
-                block.id = Math.floor(Math.random() * 100);
-                console.log("ran: " + Math.floor(Math.random() * 100));
-                localStorage.setItem("id", block.id);
-                console.log(block.id);
+                block.id = Math.floor(Math.random() * 100); //getting random id form 1-100
+                localStorage.setItem("id", block.id); //saving value in local storage
             });
-            block.addEventListener("click", () => {
+
+            //hovering over gameboard to indicate placement
+            block.addEventListener("mouseenter", () => { 
+                let next = block.nextElementSibling; 
+                let prev = block.previousElementSibling;
+
+                if(placementIsValid(block)){
+                    block.style.backgroundColor = "pink";
+                    prev.style.backgroundColor = "pink";
+                    next.style.backgroundColor = "pink";
+                }else{
+                    block.style.backgroundColor = "red";
+                    prev.style.backgroundColor = "red";
+                    next.style.backgroundColor = "red";
+                }
+            });
+
+            //reseting color after leaving gameboard
+            block.addEventListener("mouseleave", () => {
                 let next = block.nextElementSibling;
                 let prev = block.previousElementSibling;
+
+                next.style.backgroundColor = "aqua";
+                prev.style.backgroundColor = "aqua";
+                block.style.backgroundColor = "aqua";
+            });
+
+            //if placement is not valid, dont save value else save it in localstorage
+            block.addEventListener("click", () => {
+                if(!placementIsValid(block)){ 
+                    return;
+                }
+
+                let next = block.nextElementSibling;
+                let prev = block.previousElementSibling;
+
                 localStorage.setItem("prevId", prev.id);
                 localStorage.setItem("id", block.id);
                 localStorage.setItem("nextId", next.id);
-                // block.style.display = "hidden";
+                
                 block.style.backgroundColor= "orange";
                 prev.style.backgroundColor= "orange";
                 next.style.backgroundColor= "orange";
-                
-                console.log(block.id);
             });
 
+            //button 2
             ship2.addEventListener("click", () => {
                 block.id = Math.floor(Math.random() * 100);
+                
                 let next = block.nextElementSibling;
-                
-                localStorage.setItem("ship2Id2", block.id);
-                localStorage.setItem("ship2Id3", next.id);
-                
-                console.log(next.id);
+
+                next.id = block.id++;
+
+                localStorage.setItem("ship2Id1", block.id);
+                localStorage.setItem("ship2Id2", next.id);
             });
 
+            //button 3
             ship3.addEventListener("click", () => {
                 block.id = Math.floor(Math.random() * 100);
                 let next = block.nextElementSibling;
                 let prev = block.previousElementSibling;
+                
+                next.id = block.id++;
+                prev.id = block.id++;
+
                 localStorage.setItem("ship3Id1", prev.id);
                 localStorage.setItem("ship3Id2", block.id);
                 localStorage.setItem("ship3Id3", next.id);
-                console.log("next: " + next.id)
-                
-                console.log(next.id);
             });
-
         }
     }
+}
+
+//checks if placement is valid or not
+function placementIsValid(block){
+    return !(block.id % 10 < 1 || block.id % 10 > 8);
 }
